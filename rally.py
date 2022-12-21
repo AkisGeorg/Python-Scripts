2d90a8e073de7c1d6bc78df7a6dcd678e4b335c8
AkisGeorg <121067096+AkisGeorg@users.noreply.github.com>
2022-12-21 02:18:06 +0200

    Add files via upload

---
+++ rally.py
@@ -0,0 +1,30 @@
+total_drivers = 0
+attempts = 0
+
+min_name = ""
+min_time = 181
+
+for i in range(4):
+    name = input("Όνομα οδηγού: ")
+    time = int(input("Χρόνος οδηγού σε δευτερόλεπτα: "))
+    if time <= 180:
+        print(f"\nO οδηγός {name} με χρόνο {time} δευτερόλεπτα, μπορεί να συμμετάσχει στον αγώνα.\n")
+        total_drivers += 1
+    elif time > 180:
+        attempts += 1
+        print("ΜΗ ΣΥΜΜΕΤΟΧΗ")
+    else:
+        print("ΜΗ ΣΥΜΜΕΤΟΧΗ")
+
+    if time <= min_time:
+        min_name = name
+        min_time = time
+
+print("=========================================")
+print(f"Ο χαμηλότερος χρόνος είναι αυτός του {min_name} με χρόνο {min_time} δευτερόλεπτα, από το σύνολο των {total_drivers} οδηγών.")
+print("=========================================")
+
+
+
+
+
